3259e35ffc6f98fdddb401c1840c2b6d827eb162
harsh <harsh@LAPTOP-OFLH7VST>
2020-12-18 16:06:51 +0530

    New repo commit

---
+++ DemoApp/src/com/lti/model/Calc.java
@@ -0,0 +1,19 @@
+/*
+Author: HarshitVyas07 <harshitvyas07@gmail.com> 
+*/
+package com.lti.model;
+
+public class Calc {
+	public void add(int num1, int num2) {
+		System.out.println("Sum = " + (num1 + num2));
+	}
+
+	public void subs(int num1, int num2) {
+		System.out.println("Difference = " + (num1 - num2));
+	}
+
+	public void prod(int num1, int num2) {
+		System.out.println("Product = " + (num1 * num2));
+	}
+
+}
